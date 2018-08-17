e495448b3a0a0ef4bdb96cde7de4f1ca17790069
Alberto Escala <albertoaesc@gmail.com>
2018-08-16 23:24:54 -0500

    feat(src): add route.js
    you can add routes as much you want on the routes.js

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { CoverPage } from 'components';
+
+const Home = () => <CoverPage />;
+
+export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: './src/App.jsx'
+    main: './src/index.jsx'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -38,7 +38,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
-      components: path.resolve(__dirname, 'src/components/index')
+      components: path.resolve(__dirname, 'src/components/index'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      config: path.resolve(__dirname, 'src/config')
     }
   },
   plugins: [
